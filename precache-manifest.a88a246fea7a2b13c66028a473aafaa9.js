052452450a4b49c8159a9ed3918860f90df23de1
Daniel Yosilevich <danielyosilevich@gmail.com>
2019-11-12 20:35:59 +0200

    Updates

---
+++ precache-manifest.a88a246fea7a2b13c66028a473aafaa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7116feca73d1077912086af5ae6380f9",
+    "url": "/wrx/index.html"
+  },
+  {
+    "revision": "0e7e824eb1c99bdc0a28",
+    "url": "/wrx/static/css/main.61ccbab1.chunk.css"
+  },
+  {
+    "revision": "7b4fc93d29f43fc35a6b",
+    "url": "/wrx/static/js/2.c7d0238e.chunk.js"
+  },
+  {
+    "revision": "0e7e824eb1c99bdc0a28",
+    "url": "/wrx/static/js/main.b34fd3b5.chunk.js"
+  },
+  {
+    "revision": "ba026df48d83a5f5865a",
+    "url": "/wrx/static/js/runtime-main.67d99205.js"
+  }
+]);
