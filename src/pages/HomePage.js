506c87fe465e83e7feaa9265041954fe811fb603
Daniel Yosilevich <danielyosilevich@gmail.com>
2019-11-13 10:47:12 -0700

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,61 @@
 import React from 'react'
-import useGlobal from '../store/store'
+import ReactAutocomplete from "react-autocomplete";
 
-const Home = () => {
-    const [globalState] = useGlobal();
+class Home extends React.Component {
 
-    return (
-        <div className="central-wrapper">
-           <div>HomePage!</div>
-        </div>
-    )
+    state = {
+        value: '',
+        // toShow: false,
+        children: []
+    }
+
+    appendChild(val){
+        var c = this.state.children;
+        this.setState({
+            children: [
+                ...c,
+                <button>{val}</button>
+            ]
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <ReactAutocomplete
+                    items={[
+                        { id: 'foo', label: 'foo' },
+                        { id: 'bar', label: 'bar' },
+                        { id: 'baz', label: 'baz' },
+                    ]}
+                    shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                    getItemValue={item => item.label}
+                    renderItem={(item, highlighted) =>
+                        <div
+                            key={item.id}
+                            style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}
+                        >
+                            {item.label}
+                        </div>
+                    }
+                    value={this.state.value}
+                    onChange={e => this.setState({ value: e.target.value })}
+                    onSelect={value => {
+                        this.setState({ value: value, toShow: true });
+                        this.appendChild(value);
+                    }
+                    }
+                />
+
+                <div className="box-container">
+                    {this.state.children.map(child => child)}
+                </div>
+            </React.Fragment>
+        )
+    }
+
 }
+
+
 
 export default Home
